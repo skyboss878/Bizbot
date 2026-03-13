9d6480fd10220a00cc7cfedc15505232e7d77ea5
skyboss878 <elliottnathan800@gmail.com>
2026-03-12 22:44:17 -0700

    Fix MongoDB SSL for Render

--- backend/server.py
+++ backend/server.py
@@ -30,7 +30,8 @@ load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
 mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(mongo_url)
+import certifi
+client = AsyncIOMotorClient(mongo_url, tlsCAFile=certifi.where())
 db = client[os.environ['DB_NAME']]
 
 # JWT Configuration
